72afffdb241916b57928c8d8f53b77cd39049831
Cyber Boys <142109781+Dark-Legends@users.noreply.github.com>
2024-07-19 20:34:15 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,147 @@
+#!/bin/bash
+red="\033[1;31m"
+green="\033[1;32m"
+yellow="\033[1;33m"
+purple="\033[1;34m"
+pink="\033[1;35m"
+blue="\033[1;36m"
+white="\033[1;37m"
+black="\033[1;30m"
+
+banner() {
+  echo -e "\033[1;38;5;220m"
+  echo -e "\t⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+  echo -e "\t⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀⢀⣠⠴⢞⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+  echo -e "\t⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⣲⣿⡄⢀⡴⠋⣠⣾⣿⣿⣿⣿⣄⣀⣀⡀⠀⠀⠀⠀⠈⠓⠲⣦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+  echo -e "\t⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠎⣼⣿⣿⣿⡟⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣶⣦⣄⡀⠀⠀⠙⠻⣦⡀⠀⠀⠀⠀⠀⠀⠀"
+  echo -e "\t⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡿⠊⠉⠀⠀⠈⠉⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣄⡉⠛⢿⣷⣄⠀⠀⠈⢷⡄⠀⠀⠀⠀⠀⠀"
+  echo -e "\t⠀⠀⠀⠀⣀⡀⠤⠐⠒⡁⢴⣤⣤⡀⠀⠀⠀⣠⣼⣿⣿⣏⣀⣀⠀⠉⠛⢿⣿⣿⣿⣿⣿⣷⣄⠙⢿⣷⣄⠀⠀⠹⡄⠀⠀⠀⠀⠀"
+  echo -e "\t⢰⣦⣍⣁⠀⠀⠀⠀⠀⠀⢾⡉⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣦⡀⠀⢸⣿⣿⣿⣿⣿⣿⣷⡄⢻⣿⣆⠀⠀⠑⠀⠀⠀⠀⠀"
+  echo -e "\t⠈⠀⠀⠀⠀⠀⠀⠀⠠⠀⠀⠁⠁⠀⠀⠀⠀⢹⣿⣿⣿⣿⣿⣿⡿⠋⢀⣠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⢻⣿⡄⠀⠀⠁⠀⠀⠀⠀"
+  echo -e "\t⠀⠀⠀⠀⠀⠀⠀⠀⠀⢱⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⡻⢿⣿⣷⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣎⣿⣷⠀⠀⠀⠀⠀⠀⠀"
+  echo -e "\t⢀⠀⠀⠀⠀⠀⠀⠀⢸⠾⠀⢀⣠⣶⠀⣠⣾⣿⣿⣿⣿⣦⡉⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣿⣇⠀⠀⠀⠀⠀⠀"
+  echo -e "\t⠀⢹⠁⠰⠉⠉⠒⢄⣸⡷⠿⠛⠉⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⢿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀"
+  echo -e "\t⠀⠀⢇⡇⠀⠀⠀⠘⠁ ⠀⠀ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠈⣿⣿⣿⣿⣿⣿⣦⡀⠀⠀⠀"
+  echo -e "\t⠀⠀⠀⠁⠀⠀⠀⠀⠀ ⠀⠀ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⣿⣿⣯⠻⢿⣿⣿⣿⣷⣶⡆"
+  echo -e "\t⠀⠀⠀⠀⠀⠀⠀⠀  ⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⢀⣿⣿⣿⣷⣶⣶⣾⣿⡿⠟⠀"
+  echo -e "\t⠀⠀⠀⠀⠀⠀⠀⠀⢰ ⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⣼⣿⣿⣿⣿⣿⡍⠁⠀⠀⠀⠀"
+  echo -e "\t⠀⠀⢀⠞⡄⠀⠀⢠⣿⣣⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⠏⣼⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀"
+  echo -e "\t⠀⠀⢸⣀⣇⠠⠔⢫⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢫⣾⣿⣿⣿⡿⢣⣿⣿⡇⠀⠀⠀⠀⠀"
+  echo -e "\t⠀⠀⠰⡀⠀⠀⠀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢇⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⠟⢁⣾⣿⣿⠁⠀⠀⠀⠀⠀"
+  echo -e "\t⠀⠀⠀⠑⡄⢸⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⢁⣴⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀"
+  echo -e "\t⠀⠀⠀⠀⠈⠚⠉⠉⠁⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⡇⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⢛⣉⣤⣶⣿⣿⣿⡿⠃⠀⠀⠀⠀⠀⠀⠀"
+  echo -e "\t⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⠟⡇⠈⣿⣿⣿⣿⣿⣿⣿⣿⣶⣾⣿⣿⣿⣿⣿⡿⠿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+  echo -e "\t⠀⠀⠀⠀⠀⠀⠀⠀⠀⢣⠀⠹⣿⣿⣿⣿⣿⡏⠀⣿⡀⢻⣿⣿⣿⣿⣿⣿⠟⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+  echo -e "\t⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢧⡀⠈⠻⣿⣿⣿⡇⠀⠸⣷⡀⢿⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+  echo -e "\t⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢳⣄⠀⠈⠙⠿⣿⡄⠀⠘⢿⣦⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+  echo -e "\t⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢷⣤⡀⠀⠀⠀⠀⠀⠀⠈⠛⠿⢿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+  echo -e "\t⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠳⠦⠄⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n\n\n"
+  echo -e "\t\t${red}Code ${white}By ${blue}Manjunath\n\n"
+}
+
+install_linux() {
+  mv .1_install.sh install.sh 
+  mv .1_wildcrack  wildcrack
+  ./install.sh
+  printf "\e[?25h" && sleep 1
+  exit 0
+}
+
+install_termux() {
+  mv .2-install.sh install.sh
+  mv .2-wildcrack  wildcrack
+  ./install.sh
+  printf "\e[?25h" && sleep 1
+  exit 0
+}
+
+main() {
+  # Function to display the menu
+  display_menu() {
+      printf "\e[?25l"
+      clear
+      banner
+      echo -e "\033[1;38;5;135mPlease choose an option using the arrow keys and press Enter:\033[1;37m"
+      echo ""
+      for i in ${!menu_options[@]}; do
+          if [ $i -eq $current_selection ]; then
+              echo -e "\033[1;32m${menu_options[$i]}\033[0m"
+          else
+              echo "${menu_options[$i]}"
+          fi
+      done
+  }
+
+  # Function to handle the selected option
+  handle_selection() {
+      case $current_selection in
+          0)
+              echo -e "\n\033[1;37mYou chose: \033[1;32mInstall On Linux\n"
+              install_linux
+              sleep 1
+              ;;
+          1)
+              echo -e "\n\033[1;37mYou chose: \033[1;32mInstall On Termux\n"
+              install_termux
+              sleep 1
+              ;;
+          2)
+              echo -e "\033[1;37mYou chose: \033[1;32mAbout tool\033[1;37m\n"
+	      echo -e "\033[1;37mWildcrack \033[1;33mVersion \033[1;31m5.O\n"
+	      echo -e "\033[1;37mDeveloped by : \033[1;36mManjunath\n"
+ 	      echo -e "\033[1;37mWebsite Url : \033[1;32mhttps://github.com/Dark-Legends/Wild_crack.git\n"
+              sleep 2 && printf "\e[?25h"
+	      exit 0
+              ;;
+          3)
+              echo -e "\nExiting...\n"
+	      printf "\e[?25h"
+              sleep 1
+              exit 0
+              ;;
+      esac
+  }
+
+  # Initial menu options
+  menu_options=(
+      "	[   Install On Linux    ]"
+      "	[   Install On Termux   ]"
+      "	[   About tool   	]"
+      "	[   Exit         	]"
+  )
+
+  # Initial cursor position
+  current_selection=0
+
+  # Display the initial menu
+  display_menu
+
+  # Read user input
+  while true; do
+      read -rsn1 input
+      case $input in
+          $'\x1b')  # if the input is an escape character
+              read -rsn2 -t 0.1 input  # read the next two characters
+              if [[ $input == "[A" ]]; then  # up arrow
+                  ((current_selection--))
+                  if [ $current_selection -lt 0 ]; then
+                      current_selection=$(( ${#menu_options[@]} - 1 ))
+                  fi
+              elif [[ $input == "[B" ]]; then  # down arrow
+                  ((current_selection++))
+                  if [ $current_selection -ge ${#menu_options[@]} ]; then
+                      current_selection=0
+                  fi
+              fi
+              ;;
+          "")  # if the input is Enter
+              handle_selection
+              # Re-display the menu after handling the selection
+              display_menu
+              ;;
+      esac
+      display_menu
+  done
+}
+
+main
